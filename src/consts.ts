3a509f9a93b3d7717d5c72e4e88915c55088fd5f
axelbaron <axelbaron@outlook.fr>
2025-03-13 19:35:20 +0100

    :sparkles: initial commit, use astro blog

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Groove Lab Media';
+export const SITE_DESCRIPTION = 'Fueling your digital rhythm! Discover new groove music and captivating content.';
